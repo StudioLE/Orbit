b985a333e75a81551631a720f272d866eabcbd32
Laurence Elsdon <git@elsdon.me>
2023-04-04 12:19:12 +0000

    Convert create-boot-disk to a function and add a test

---
+++ lib/boot-disk/tests/create-boot-disk.test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -uo pipefail
+
+# ARGS
+ACTUAL_FILE=$1
+
+# SETUP
+
+source /srv/lib/boot-disk/create-boot-disk.sh
+
+# START
+
+create-boot-disk "ubuntu" "jammy" "20G"
+echo "${RETURN_VALUE}" > "${ACTUAL_FILE}"
