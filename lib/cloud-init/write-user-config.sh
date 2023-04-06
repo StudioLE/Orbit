183dbe264183924e0785b8b2c984bed2d889332e
Laurence Elsdon <git@elsdon.me>
2023-04-06 13:31:33 +0000

    Install docker once per instance

--- lib/cloud-init/write-user-config.sh
+++ lib/cloud-init/write-user-config.sh
@@ -29,18 +29,24 @@ function write-user-config {
   # START
 
   OUTPUT=$(cat "${CONFIG_SRC}")
-  write-yaml-string "${OUTPUT}" "\${HOSTNAME}" "${HOSTNAME}"
-  write-yaml-string "${OUTPUT}" "\${USER}" "${USER}"
-  write-yaml-string "${OUTPUT}" "\${SUDO_USER}" "${SUDO_USER}"
+
+  # Blocks
+  INSTALL_DOCKER_CONTENT=$(cat "/srv/lib/cloud-init/src/10-install-docker.sh")
+  write-yaml-block "${OUTPUT}" "\${INSTALL_DOCKER_CONTENT}" "${INSTALL_DOCKER_CONTENT}"
+
+  # Arrays
   SSH_AUTHORIZED_KEYS=$(cat "/root/.ssh/id_rsa.pub")
   write-yaml-array "${OUTPUT}" "\${SSH_AUTHORIZED_KEYS}" "${SSH_AUTHORIZED_KEYS}" 3
-  write-yaml-string "${OUTPUT}" "\${SSH_PORT}" "${SSH_PORT}"
-  HELLO_WORLD_CONTENT=$(cat "/srv/lib/cloud-init/src/50-hello-world")
-  write-yaml-block "${OUTPUT}" "\${HELLO_WORLD_CONTENT}" "${HELLO_WORLD_CONTENT}"
   BOOTCMD_CONTENT=$(cat "/srv/lib/cloud-init/src/bootcmd.sh")
   write-yaml-array "${OUTPUT}" "\${BOOTCMD_CONTENT}" "${BOOTCMD_CONTENT}" 1
   RUNCMD_CONTENT=$(cat "/srv/lib/cloud-init/src/runcmd.sh")
   write-yaml-array "${OUTPUT}" "\${RUNCMD_CONTENT}" "${RUNCMD_CONTENT}" 1
+
+  # Strings
+  write-yaml-string "${OUTPUT}" "\${HOSTNAME}" "${HOSTNAME}"
+  write-yaml-string "${OUTPUT}" "\${USER}" "${USER}"
+  write-yaml-string "${OUTPUT}" "\${SUDO_USER}" "${SUDO_USER}"
+  write-yaml-string "${OUTPUT}" "\${SSH_PORT}" "${SSH_PORT}"
 
 }
 
