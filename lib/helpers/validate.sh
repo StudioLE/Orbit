3a8bdccac922197bc84d00fefd22e42fd3889043
Laurence Elsdon <git@elsdon.me>
2023-04-06 09:05:13 +0000

    WIP added validate.sh

--- lib/helpers/validate.sh
+++ lib/helpers/validate.sh
@@ -7,17 +7,31 @@ function validate {
 
   source /srv/lib/helpers/echo-error.sh
 
-  # START
+  # ARGS
 
   if [[ ! -v "1" ]]
   then
     echo-error "validate: \$1 was not set."
     exit 1
-  
-  elif [[ "$1" =~ ^-?[0-9]+$ ]]
+  elif [[ "$1" == "" ]]
   then
-    echo "TRUE"
+    echo-error "validate: \$1 was empty."
+    exit 1
+  fi
+
+  # START
+
+  declare COMMAND=$1
+  set +e
+  OUTPUT=$(${COMMAND} 2>&1)
+  EXIT_CODE=$?
+  set -e
+
+  if [[ "${EXIT_CODE}" != 0 ]]
+  then
+    echo-error "${OUTPUT}"
+    exit ${EXIT_CODE}
   else
-    echo ""
+    echo "${OUTPUT}"
   fi
 }
