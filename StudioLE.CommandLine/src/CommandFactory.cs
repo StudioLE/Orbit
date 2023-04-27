473171f36b0009acc67c49f69a716b69821fcfe9
Laurence Elsdon <git@elsdon.me>
2023-04-27 21:41:03 +0100

    refactor(StudioLE.CommandLine): Extract logic to CommandOptionsFactory

--- StudioLE.CommandLine/src/CommandFactory.cs
+++ StudioLE.CommandLine/src/CommandFactory.cs
@@ -2,27 +2,25 @@ using System.CommandLine;
 using System.CommandLine.Binding;
 using System.CommandLine.Invocation;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using StudioLE.CommandLine.Composition;
 using StudioLE.CommandLine.Utils;
-using StudioLE.Core.System;
 
 namespace StudioLE.CommandLine;
 
 public class CommandFactory
 {
     private readonly IHostBuilder _hostBuilder;
-    private readonly HashSet<Type> _parseableTypes =  new()
-        {
-            typeof(string),
-            typeof(int),
-            typeof(double),
-            typeof(Enum)
-        };
-    private readonly HashSet<string> _optionAliases = new();
+    private readonly HashSet<Type> _parseableTypes = new()
+    {
+        typeof(string),
+        typeof(int),
+        typeof(double),
+        typeof(Enum)
+    };
+    private readonly CommandOptionsFactory _optionsFactory;
     private readonly Dictionary<string, Option> _options = new();
 
     public Type? ActivityType { get; set; }
@@ -34,12 +32,14 @@ public class CommandFactory
     public CommandFactory(IHostBuilder hostBuilder)
     {
         _hostBuilder = hostBuilder;
+        _optionsFactory = new(IsParseable);
     }
 
     public CommandFactory(IHostBuilder hostBuilder, HashSet<Type> parseableTypes)
     {
         _hostBuilder = hostBuilder;
         _parseableTypes = parseableTypes;
+        _optionsFactory = new(IsParseable);
     }
 
     public Command Build()
@@ -50,7 +50,8 @@ public class CommandFactory
         Command command = new(CommandName!, Description);
         MethodInfo activityMethod = ActivityType.GetMethod("Execute") ?? throw new("No Execute method");
         ObjectTree tree = CreateObjectTree(activityMethod);
-        Option[] options = CreateOptionsForProperties(tree);
+
+        Option[] options = _optionsFactory.Create(tree);
         foreach (Option option in options)
         {
             _options.Add(option.Aliases.First(), option);
@@ -72,17 +73,6 @@ public class CommandFactory
             : descriptionAttribute.Description;
     }
 
-    private Action<InvocationContext> CreateHandler(Type activityType, MethodInfo activityMethod, ObjectTree tree)
-    {
-        return context =>
-        {
-            object parameter = GetOptionValue(context.BindingContext, tree);
-            IHost host = _hostBuilder.Build();
-            object activity = host.Services.GetRequiredService(activityType);
-            activityMethod.Invoke(activity, new [] { parameter });
-        };
-    }
-
     private ObjectTree CreateObjectTree(MethodInfo method)
     {
         ParameterInfo[] parameters = method.GetParameters();
@@ -92,84 +82,21 @@ public class CommandFactory
         return new(parameter.ParameterType);
     }
 
-    private Option[] CreateOptionsForProperties(ObjectTree tree)
+    private Action<InvocationContext> CreateHandler(Type activityType, MethodInfo activityMethod, ObjectTree tree)
     {
-         return tree
-            .FlattenProperties()
-            .Where(x => IsParseable(x.Type))
-            .Select(CreateOptionForProperty)
-            .ToArray();
+        return context =>
+        {
+            object parameter = GetOptionValue(context.BindingContext, tree);
+            IHost host = _hostBuilder.Build();
+            object activity = host.Services.GetRequiredService(activityType);
+            activityMethod.Invoke(activity, new[] { parameter });
+        };
     }
 
     private bool IsParseable(Type type)
     {
         return _parseableTypes.Contains(type)
                || _parseableTypes.Any(x => x.IsAssignableFrom(type));
-    }
-
-    private Option CreateOptionForProperty(ObjectTreeProperty tree)
-    {
-        IReadOnlyCollection<string> aliases = GetAliases(tree);
-        Option option = CreateInstanceOfOption(tree);
-        SetOptionValidator(tree, option);
-        foreach (string alias in aliases)
-            _optionAliases.Add(alias);
-        return option;
-    }
-
-    private IReadOnlyCollection<string> GetAliases(ObjectTreeProperty tree)
-    {
-        HashSet<string> aliases = new()
-        {
-            tree.FullKey.ToLongOption()
-        };
-        if(!_optionAliases.Contains(tree.Key.ToLongOption()))
-            aliases.Add(tree.Key.ToLongOption());
-        if (tree.Parent is ObjectTreeProperty parent)
-        {
-            if(!_optionAliases.Contains(parent.FullKey.ToLongOption()))
-                aliases.Add(parent.FullKey.ToLongOption());
-            if(!_optionAliases.Contains(parent.Key.ToLongOption()))
-                aliases.Add(parent.Key.ToLongOption());
-        }
-        return aliases;
-    }
-
-    private Option CreateInstanceOfOption(ObjectTreeProperty tree)
-    {
-        IReadOnlyCollection<string> aliases = GetAliases(tree);
-        Type optionType = typeof(Option<>).MakeGenericType(tree.Type);
-        object instance = Activator.CreateInstance(optionType, aliases.ToArray(), tree.HelperText) ?? throw new("Failed to construct option. Activator returned null.");
-        if (instance is not Option option)
-            throw new("Failed to construct option. Activator didn't return an Option.");
-        return option;
-    }
-
-    private static void SetOptionValidator(ObjectTreeProperty tree, Option option)
-    {
-        ValidationAttribute[] validationAttributes = tree
-            .Property
-            .GetCustomAttributes<ValidationAttribute>()
-            .ToArray();
-        if (!validationAttributes.Any())
-            return;
-        option.AddValidator(result =>
-        {
-            object? value = result.GetValueForOption(option);
-            List<ValidationResult> results = new();
-            ValidationContext context = new(value!)
-            {
-                DisplayName = result.Token?.Value ?? throw new("Failed to get the value of the token.")
-                // DisplayName = option.Description ?? throw new("Failed to get the value of the token.")
-            };
-            if (Validator.TryValidateValue(value!, context, results, validationAttributes))
-                return;
-            string message = results
-                .Select(x => x.ErrorMessage)
-                .OfType<string>()
-                .Join();
-            result.ErrorMessage = message;
-        });
     }
 
     private object GetOptionValue(BindingContext context, ObjectTree tree)
@@ -179,14 +106,14 @@ public class CommandFactory
             .ToArray();
         foreach (ObjectTreeProperty factory in propertyFactories)
         {
-            if(!IsParseable(factory.Type))
+            if (!IsParseable(factory.Type))
                 continue;
             if (!_options.TryGetValue(factory.FullKey.ToLongOption(), out Option? option))
                 continue;
             object? value = context.ParseResult.GetValueForOption(option);
             if (value is null)
                 continue;
-            if(!factory.Type.IsInstanceOfType(value))
+            if (!factory.Type.IsInstanceOfType(value))
                 continue;
             factory.SetValue(value);
         }
---
+++ StudioLE.CommandLine/src/CommandOptionsFactory.cs
@@ -0,0 +1,92 @@
+using System.CommandLine;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using StudioLE.CommandLine.Composition;
+using StudioLE.Core.System;
+
+namespace StudioLE.CommandLine;
+
+public class CommandOptionsFactory
+{
+    private readonly Func<Type, bool> _isParseable;
+    private readonly HashSet<string> _optionAliases = new();
+
+    public CommandOptionsFactory(Func<Type,bool> isParseable)
+    {
+        _isParseable = isParseable;
+    }
+
+    public Option[] Create(ObjectTree tree)
+    {
+         return tree
+            .FlattenProperties()
+            .Where(x => _isParseable.Invoke(x.Type))
+            .Select(CreateOptionForProperty)
+            .ToArray();
+    }
+
+    private Option CreateOptionForProperty(ObjectTreeProperty tree)
+    {
+        IReadOnlyCollection<string> aliases = GetAliases(tree);
+        Option option = CreateInstanceOfOption(tree);
+        SetOptionValidator(tree, option);
+        foreach (string alias in aliases)
+            _optionAliases.Add(alias);
+        return option;
+    }
+
+    private IReadOnlyCollection<string> GetAliases(ObjectTreeProperty tree)
+    {
+        HashSet<string> aliases = new()
+        {
+            tree.FullKey.ToLongOption()
+        };
+        if(!_optionAliases.Contains(tree.Key.ToLongOption()))
+            aliases.Add(tree.Key.ToLongOption());
+        if (tree.Parent is ObjectTreeProperty parent)
+        {
+            if(!_optionAliases.Contains(parent.FullKey.ToLongOption()))
+                aliases.Add(parent.FullKey.ToLongOption());
+            if(!_optionAliases.Contains(parent.Key.ToLongOption()))
+                aliases.Add(parent.Key.ToLongOption());
+        }
+        return aliases;
+    }
+
+    private Option CreateInstanceOfOption(ObjectTreeProperty tree)
+    {
+        IReadOnlyCollection<string> aliases = GetAliases(tree);
+        Type optionType = typeof(Option<>).MakeGenericType(tree.Type);
+        object instance = Activator.CreateInstance(optionType, aliases.ToArray(), tree.HelperText) ?? throw new("Failed to construct option. Activator returned null.");
+        if (instance is not Option option)
+            throw new("Failed to construct option. Activator didn't return an Option.");
+        return option;
+    }
+
+    private static void SetOptionValidator(ObjectTreeProperty tree, Option option)
+    {
+        ValidationAttribute[] validationAttributes = tree
+            .Property
+            .GetCustomAttributes<ValidationAttribute>()
+            .ToArray();
+        if (!validationAttributes.Any())
+            return;
+        option.AddValidator(result =>
+        {
+            object? value = result.GetValueForOption(option);
+            List<ValidationResult> results = new();
+            ValidationContext context = new(value!)
+            {
+                DisplayName = result.Token?.Value ?? throw new("Failed to get the value of the token.")
+                // DisplayName = option.Description ?? throw new("Failed to get the value of the token.")
+            };
+            if (Validator.TryValidateValue(value!, context, results, validationAttributes))
+                return;
+            string message = results
+                .Select(x => x.ErrorMessage)
+                .OfType<string>()
+                .Join();
+            result.ErrorMessage = message;
+        });
+    }
+}
