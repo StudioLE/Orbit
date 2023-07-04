ac5093b4a0ac9f1600dc68e70ccc0cf5efaaff75
Laurence Elsdon <git@elsdon.me>
2023-07-04 20:22:47 +0100

    feat(Orbit.Core): Remove netplan apply logic

--- Orbit.Core/src/Resources/Scripts/bootcmd.sh
+++ Orbit.Core/src/Resources/Scripts/bootcmd.sh
@@ -1,9 +1,7 @@
 echo "*********************************"
 echo "Executing bootcmd"
 echo "*********************************"
-if [ ! -f /root/first-run-complete ]; then echo "IS FIRST RUN"; else echo "NOT FIRST RUN"; fi
-if [ ! -f /root/first-run-complete ]; then netplan apply; fi
 echo "###### IP Addresses"
-ip -c -br addr
+ip -br addr
 echo "###### IP Routes"
-ip -c route
+ip route
--- Orbit.Core/src/Resources/Scripts/runcmd.sh
+++ Orbit.Core/src/Resources/Scripts/runcmd.sh
@@ -2,4 +2,3 @@ echo "*********************************"
 echo "Executing runcmd"
 echo "*********************************"
 systemctl restart ssh
-touch /root/first-run-complete
