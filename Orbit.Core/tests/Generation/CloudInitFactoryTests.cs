9497bd2df205957645cefa5195cbffcdc728fe55
Laurence Elsdon <git@elsdon.me>
2023-07-27 14:39:26 +0100

    test: Added test for CloudInitFactory

---
+++ Orbit.Core/tests/Generation/CloudInitFactoryTests.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NUnit.Framework;
+using Orbit.Core.Generation;
+using Orbit.Core.Schema;
+using Orbit.Core.Utils.Logging.TestLogger;
+using StudioLE.Verify;
+using StudioLE.Verify.NUnit;
+
+namespace Orbit.Core.Tests.Generation;
+
+internal sealed class CloudInitFactoryTests
+{
+    private readonly IVerify _verify = new NUnitVerify();
+    private readonly CloudInitFactory _factory;
+    private readonly IReadOnlyCollection<TestLog> _logs;
+
+    public CloudInitFactoryTests()
+    {
+        #if DEBUG
+        Environment.SetEnvironmentVariable("DOTNET_ENVIRONMENT", "Development");
+        #endif
+        IHost host = TestHelpers.CreateTestHost();
+        _factory = host.Services.GetRequiredService<CloudInitFactory>();
+        _logs = host.Services.GetTestLogs();
+    }
+
+    [Test]
+    [Category("Factory")]
+    public async Task CloudInitFactory_Create()
+    {
+        // Arrange
+        Instance instance = TestHelpers.ExampleInstance();
+
+        // Act
+        string output = _factory.Create(instance);
+
+        // Assert
+        Assert.That(_logs.Count, Is.EqualTo(0), "Logs Count");
+        await _verify.String(output);
+    }
+}
